a12686df5318a92a64a560dfc6da800c645803ca
BriceBchd <blanch_b@etna-alternance.net>
2022-08-16 12:49:20 +0200

    correction with java eclipse

--- Etape_1/src/main/java/com/quest/etna/QuestWebJavaApplication.java
+++ Etape_1/src/main/java/com/quest/etna/QuestWebJavaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class QuestWebApplication {
+public class QuestWebJavaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(QuestWebApplication.class, args);
+		SpringApplication.run(QuestWebJavaApplication.class, args);
 	}
 
 }
--- Etape_1/src/main/java/com/quest/etna/controller/DefaultController.java
+++ Etape_1/src/main/java/com/quest/etna/controller/DefaultController.java
@@ -7,18 +7,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DefaultController {
-	
+
 	@GetMapping("/testSuccess")
 	public ResponseEntity<String> testSuccess() {
 		String response = "success";
 		return new ResponseEntity<>(response, HttpStatus.OK);
 	}
-	
+
 	@GetMapping("/testNotFound")
 	public ResponseEntity<String> testNotFound() {
 		return new ResponseEntity<>("not found", HttpStatus.NOT_FOUND);
 	}
-	
+
 	@GetMapping("/testError")
 	public ResponseEntity<String> testError() {
 		return new ResponseEntity<>("error", HttpStatus.INTERNAL_SERVER_ERROR);
