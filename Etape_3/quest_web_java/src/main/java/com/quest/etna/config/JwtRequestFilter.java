f2951c50ec456dff1188c390401aaf4d572ae1c6
Brice Bchd <blanch_b@etna-alternance.net>
2022-08-24 19:41:44 +0200

    add JwtRequestFilter step 2e

---
+++ Etape_3/quest_web_java/src/main/java/com/quest/etna/config/JwtRequestFilter.java
@@ -0,0 +1,66 @@
+package com.quest.etna.config;
+
+import com.quest.etna.model.JwtUserDetails;
+import com.quest.etna.repositories.UserRepository;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtRequestFilter extends OncePerRequestFilter {
+    private final JwtTokenUtil jwtTokenUtil;
+    private final UserRepository userRepository;
+
+    public JwtRequestFilter(JwtTokenUtil jwtTokenUtil,
+                          UserRepository userRepository) {
+        this.jwtTokenUtil = jwtTokenUtil;
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain chain)
+            throws ServletException, IOException {
+        // Get authorization header and validate
+        final String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (!header.startsWith("Bearer ")) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        // Get jwt token
+        final String token = header.split(" ")[1].trim();
+
+        // Get user identity and set it on the spring security context
+        JwtUserDetails userDetails = new JwtUserDetails(userRepository
+                .findByUsername(jwtTokenUtil.getUsernameFromToken(token)));
+
+        //validate token
+        if (Boolean.FALSE.equals(jwtTokenUtil.validateToken(token, userDetails))) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        UsernamePasswordAuthenticationToken
+                authentication = new UsernamePasswordAuthenticationToken(
+                userDetails, null,
+                userDetails.getAuthorities()
+        );
+
+        authentication.setDetails(
+                new WebAuthenticationDetailsSource().buildDetails(request)
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        chain.doFilter(request, response);
+    }
+}
