0074fc54e1a60082f7b2f77a5dd08015846fea11
BriceBchd <blanch_b@etna-alternance.net>
2022-09-03 10:34:52 +0200

    add junit first test

---
+++ Etape_4/quest_web_java/src/test/java/com/quest/etna/ControllerTests.java
@@ -0,0 +1,39 @@
+package com.quest.etna;
+
+import org.json.JSONObject;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ControllerTests {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Test
+    public void testAuthenticate() throws Exception{
+        JSONObject jsonBody = new JSONObject();
+        jsonBody.put("username", "toto");
+        jsonBody.put("password", "1234");
+        mockMvc.perform(MockMvcRequestBuilders.post("/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonBody.toString()))
+                .andExpect(status().isCreated());
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonBody.toString()))
+                .andExpect(status().isConflict());
+    }
+}
