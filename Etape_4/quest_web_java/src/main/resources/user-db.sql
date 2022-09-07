048d97e18f82edbc973f9dbf3fc7297be621ac3b
Brice Bchd <blanch_b@etna-alternance.net>
2022-09-07 19:38:38 +0200

    test for address all working

--- Etape_4/quest_web_java/src/main/resources/user-db.sql
+++ Etape_4/quest_web_java/src/main/resources/user-db.sql
@@ -1,5 +1,11 @@
 SET FOREIGN_KEY_CHECKS = 0;
 TRUNCATE TABLE user;
+TRUNCATE TABLE address;
 insert into quest_web.user (id, creation_date, password, role, updated_date, username)
-values  (1, '2022-08-30 22:30:18', '$2a$10$huURGOgbVCTW0s09PwJZIOOOWF34iY/iZwddsX0S.qrfXybI6L1FW', 'ROLE_USER', null, 'BriCe3');
+values  (1, '2022-08-30 22:30:18', '$2a$10$huURGOgbVCTW0s09PwJZIOOOWF34iY/iZwddsX0S.qrfXybI6L1FW', 'ROLE_USER', null, 'Brice'),
+        (2, '2022-08-30 22:30:18', '$2a$10$huURGOgbVCTW0s09PwJZIOOOWF34iY/iZwddsX0S.qrfXybI6L1FW', 'ROLE_ADMIN', null, 'JeanAntoine');
+
+insert into quest_web.address (id, city, country, creation_date, postal_code, street, updated_date, user_id)
+values  (1, 'Gagny', 'France', '2022-09-07 09:48:56', '93220', 'rue de la mairie', '2022-09-07 09:48:56', 1),
+        (2, 'Villemomble', 'France', '2022-09-07 09:51:23', '93250', 'rue de la paix', '2022-09-07 09:51:23', 2);
 SET FOREIGN_KEY_CHECKS = 1;
--- Etape_4/quest_web_java/src/test/java/com/quest/etna/ControllerTests.java
+++ Etape_4/quest_web_java/src/test/java/com/quest/etna/ControllerTests.java
@@ -62,9 +62,52 @@ public class ControllerTests {
 //      TODO after CRUD user
     }
 
+    @Sql(scripts = "/user-db.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(scripts = "/user-db.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     @Test
     public void testAddress() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/address"))
                 .andExpect(status().isUnauthorized());
+
+        JSONObject jsonBody = new JSONObject();
+        jsonBody.put("username", "brice");
+        jsonBody.put("password", "1234");
+        MvcResult tokenResult = mockMvc.perform(MockMvcRequestBuilders.post("/authenticate")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonBody.toString()))
+                .andReturn();
+        String token = JsonPath.read(tokenResult.getResponse().getContentAsString(), "$.token");
+        mockMvc.perform(MockMvcRequestBuilders.get("/address")
+                        .header("Authorization", "Bearer " + token))
+                .andExpect(status().isOk());
+
+        jsonBody.remove("username");
+        jsonBody.remove("password");
+        jsonBody.put("street", "rue de la gare");
+        jsonBody.put("postalCode", "9220");
+        jsonBody.put("city", "GAGNY");
+        jsonBody.put("country", "FRANCE");
+        mockMvc.perform(MockMvcRequestBuilders.post("/address")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonBody.toString())
+                        .header("Authorization", "Bearer " + token))
+                .andExpect(status().isCreated());
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/address/2")
+                .header("Authorization", "Bearer " + token))
+                .andExpect(status().isForbidden());
+
+        JSONObject jsonBody2 = new JSONObject();
+        jsonBody2.put("username", "JeanAntoine");
+        jsonBody2.put("password", "1234");
+        MvcResult tokenResult2 = mockMvc.perform(MockMvcRequestBuilders.post("/authenticate")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonBody2.toString()))
+                .andExpect(status().isOk())
+                .andReturn();
+        String token2 = JsonPath.read(tokenResult2.getResponse().getContentAsString(), "$.token");
+        mockMvc.perform(MockMvcRequestBuilders.delete("/address/1")
+                        .header("Authorization", "Bearer " + token2))
+                .andExpect(status().isOk());
     }
 }
