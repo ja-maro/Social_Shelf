a7a60a1a19297080ab519b44b0688694c3e030bd
jeana <jeana@LAPTOP-AA29K9N1>
2022-09-13 11:03:25 +0200

    user getById & getAll

--- Etape_4/quest_web_java/src/main/java/com/quest/etna/controller/UserController.java
+++ Etape_4/quest_web_java/src/main/java/com/quest/etna/controller/UserController.java
@@ -1,13 +1,18 @@
 package com.quest.etna.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.quest.etna.model.User;
 import com.quest.etna.repositories.UserRepository;
@@ -19,14 +24,23 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepo;
 	
-	@GetMapping
-	public ResponseEntity<User> get() {
-		return null;
+	@GetMapping("/{id}")
+	public ResponseEntity<User> getById(@PathVariable int id) {	
+		Optional<User> dbUser = userRepo.findById(id);
+		if (dbUser.isPresent()) {
+			User user = dbUser.get();
+			user.setPassword("*****");
+			return ResponseEntity.ok(user);
+		} else {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		}
 	}
 
 	@GetMapping
-	public ResponseEntity<Iterable<User>> getAll() {
-		return null;
+	public ResponseEntity<Iterable<User>> getAll() {	
+		Iterable<User> users = userRepo.findAll();
+		users.forEach(user -> user.setPassword("*****"));
+		return ResponseEntity.ok(users);
 	}
 	
 	@PutMapping
