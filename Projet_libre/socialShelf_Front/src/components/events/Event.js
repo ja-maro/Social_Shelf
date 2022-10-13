4b64bcf7230fdd34c796ea40b62785e8453ea5be
Jean-Antoine <maro_j@etna-alternance.net>
2022-10-13 17:38:54 +0200

    API displayParticipantsList

--- Projet_libre/socialShelf_Front/src/components/events/Event.js
+++ Projet_libre/socialShelf_Front/src/components/events/Event.js
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 import EventsService from "../../services/events.service";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import EventListItem from "./EventListItem";
 
 const Event = () => {
     const [ready, setReady] = useState(false);
     const [event, setEvent] = useState({});
+    const [participantsList, setparticipantsList] = useState([]);
     const { id } = useParams();
+    const navigate = useNavigate();
 
     useEffect(() => {
         EventsService.getById(id).then((response) => {
@@ -23,10 +25,15 @@ const Event = () => {
     return ready === false ? null : (
         <div>
             <h1>Event (details)</h1>
-
+            <button onClick={() => navigate("/events/mine")}>Back to my events</button>
             <EventListItem key={event.id} event={event} />
             <br />
-            <br />
+            <h2>Participants : </h2>
+            {participantsList.map((player) => (
+                    <div key={player.id}>
+                        {player.username}
+                    </div>
+                ))}
             <br />
         </div>
     );
